01e967b34f673c569e2639d7487e707f595f0707
parvezch <parvezchoudhury3@gmail.com>
2019-03-18 19:29:17 +0530

    msm8937-common: Force deep sleep

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -142,7 +142,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.empty_app_percent=50 \
     ro.vendor.qti.sys.fw.trim_empty_percent=100 \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648 \
+    pm.sleep_mode=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
