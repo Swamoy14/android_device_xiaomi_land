62e582167312e87ac8c2372d93ff42ad0ee79e3a
GuaiYiHu <guaiyihu@foxmail.com>
2019-10-27 13:19:48 +0530

    msm8937-common: Enable QC wifi dual ap feature

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,6 +199,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
+QC_WIFI_HIDL_FEATURE_DUAL_AP := true
 
 # Inherit the common proprietary files
 -include vendor/xiaomi/msm8937-common/BoardConfigVendor.mk
