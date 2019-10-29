a4672789411f37dd193b25161251eb622142731b
SakilMondal <mondalsakil@gmail.com>
2019-10-29 11:21:04 +0530

    Revert "msm8937-common: Force disable bluetooth audio hal 2.0"
    This reverts commit 85c951249ea0bec41e1e47d882c4498cb38c3505.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,8 +48,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=smd
-    ro.qualcomm.bt.hci_transport=smd \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
