a5996b51128567e85b0874ec5c263613472dce1f
Swamoy14 <shubhasankar14@gmail.com>
2020-01-16 22:00:58 +0530

    power: use common power 1.2 hal

--- device.mk
+++ device.mk
@@ -355,7 +355,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.land
+	android.hardware.power@1.2-service-qti
     
 #Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
