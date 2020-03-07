cacac52941f73fdd7810aaa2f91dcb442152a470
Swamoy14 <shubhasankar14@gmail.com>
2020-03-07 13:13:18 +0530

    land: removed vendor framework compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,6 @@ USE_DEVICE_SPECIFIC_GPS := true
 TARGET_NO_RPC := true
 
 # HIDL
-#DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
