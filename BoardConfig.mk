4ce6b49855d425e4d909c778ff8918360e270ef7
Swamoy14 <shubhasankar14@gmail.com>
2020-03-21 23:05:15 +0530

    land: cpu variant runtime set to cortex-a53

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,8 @@ TARGET_ARCH 	    	:= arm64
 TARGET_ARCH_VARIANT 	:= armv8-a
 TARGET_CPU_ABI 		:= arm64-v8a
 TARGET_CPU_ABI2 	:=
-ARGET_CPU_VARIANT 	:= generic
+TARGET_CPU_VARIANT 	:= generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Second architecture
 TARGET_2ND_ARCH 	:= arm
@@ -31,6 +32,9 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI 	:= armeabi-v7a
 TARGET_2ND_CPU_ABI2 	:= armeabi
 TARGET_2ND_CPU_VARIANT 	:= generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Board platform
 TARGET_BOARD_PLATFORM 	  := msm8937
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
 BUILD_BROKEN_DUP_RULES := true
