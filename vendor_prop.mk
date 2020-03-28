d3849134a93c3ae0647017ef3429586ed1136e36
Han Wang <416810799@qq.com>
2020-03-28 14:34:59 +0100

    Revert "msm8937-common: enable GL comp backpressure"
     * This is no-op when debug.sf.disable_backpressure=1
       is set. Just follow CAF here and remove this ricing.
    
    Change-Id: I74c5e4ba830ef23448e4f16822687d7a22746a3b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -109,7 +109,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     sdm.debug.disable_skip_validate=1 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.enable_gl_backpressure=1 \
     sdm.perf_hint_window=50
 
 # IMS
