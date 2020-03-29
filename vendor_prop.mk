2cb5d45cd62faa97b87bf6071b267096fc04f9cc
sairam1411 <majgaonkars3@gmail.com>
2020-03-29 17:07:03 +0000

    land: Fix Recent UI lags
    After been trying to fix it for months. Finally changing the props to make it work
    Thanks to zeelog got reference from his mido tree
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -82,7 +82,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
-    ro.qualcomm.cabl=0 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=320 \
     vendor.display.enable_default_color_mode=1
@@ -101,12 +100,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.egl.hw=1 \
+    debug.egl.hw=0 \
     debug.enable.sglscale=1 \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.enable_hwc_vds=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    debug.sf.hw=1 \
+    debug.sf.hw=0 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
