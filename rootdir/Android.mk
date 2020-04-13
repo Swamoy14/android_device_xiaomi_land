7698dd50e939d730b34e9537e9bea21957a25eff
Swamoy14 <shubhasankar14@gmail.com>
2020-04-13 23:38:57 +0530

    land: fingerprint and rootdir updated

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -91,12 +91,4 @@ LOCAL_SRC_FILES    := ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.goodix.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.goodix.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 # Configuration scripts
