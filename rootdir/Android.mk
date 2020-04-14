7e68665d92b8236b46023f757954ec07817942bb
Swamoy14 <shubhasankar14@gmail.com>
2020-04-14 18:41:16 +0530

    Revert "land: fingerprint and rootdir updated"
    This reverts commit 7698dd50e939d730b34e9537e9bea21957a25eff.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -91,4 +91,12 @@ LOCAL_SRC_FILES    := ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.goodix.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.goodix.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # Configuration scripts
