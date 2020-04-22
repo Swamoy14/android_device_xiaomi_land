ef9da6c30ab590e651970e76e6762a6573896a6f
sairam1411 <majgaonkars3@gmail.com>
2020-04-22 08:41:29 +0000

    msm8937-common: Enable d2tw overlay

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,9 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
+
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
