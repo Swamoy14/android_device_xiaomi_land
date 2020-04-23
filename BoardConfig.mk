9fdbd3a677415aeae0b49bc0b6852c83ea5673ac
Swamoy14 <shubhasankar14@gmail.com>
2020-04-23 15:59:45 +0530

    sepolicy: made it permissive again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,7 @@ TARGET_NO_BOOTLOADER 		:= true
 # kernel
 BOARD_KERNEL_BASE		        := 0x80000000
 BOARD_KERNEL_CMDLINE 		:= androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.usbconfigfs=false loop.max_part=7
+BOARD_KERNEL_CMDLINE 		+= androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME 	:= Image.gz-dtb
 BOARD_KERNEL_PAGESIZE 		:=  2048
 BOARD_MKBOOTIMG_ARGS 		:= --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
@@ -233,9 +234,8 @@ TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 #Treble
 ENABLE_VENDOR_IMAGE := true
