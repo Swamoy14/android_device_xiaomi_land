0de85a062d9e3789f017db709aa3658c3d8d82c2
sairam1411 <majgaonkars3@gmail.com>
2020-04-29 04:06:41 +0000

    msm8937-common: Remove powerstats

--- msm8937.mk
+++ msm8937.mk
@@ -318,8 +318,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
