8296651375ed7b403f0094f5c26e701e12570991
sairam1411 <majgaonkars3@gmail.com>
2020-06-02 19:08:03 +0530

    msm8937-common: Update some props from stock
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,7 +126,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable.pq=true \
     vendor.vidc.enc.narrow.searchrange=1 \
     vendor.video.disable.ubwc=1 \
-    debug.media.codec2=2
+    debug.media.codec2=2 \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    media.msm8956hw=0 \
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
