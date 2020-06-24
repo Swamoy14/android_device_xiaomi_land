7f27633c52d58dec3eae06149c0e2dde10ebb9bf
Scott Warner <Tortel1210@gmail.com>
2020-06-24 12:38:56 -0400

    land: Use opensource btconfigstore
    Change-Id: Id16333c0b36fbf93e504fe6cb0107fce88721791

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku1.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
