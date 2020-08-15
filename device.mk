b50b9eae6e8c200ff599f3ff9fd1b6000031bb6c
Swamoy14 <shubhasankar14@gmail.com>
2020-08-15 11:59:00 +0530

    land: Use GCam as default camera app

--- device.mk
+++ device.mk
@@ -123,7 +123,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8937 \
-    Snap
+    GCam
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
