dde2d83e963f5c8a2e05deacf697973aec01a33c
Swamoy14 <shubhasankar14@gmail.com>
2020-08-15 11:56:42 +0530

    land: Use global DICM package

--- device.mk
+++ device.mk
@@ -212,9 +212,6 @@ PRODUCT_PACKAGES += \
     ims_ext_common.xml \
     libui_shim
 
-# IPACM Data
-$(call inherit-product, device/xiaomi/land/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
-
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
