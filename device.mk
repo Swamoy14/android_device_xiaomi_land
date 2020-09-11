00de3fd59eaf4709647ee05d70055e42956833bc
GiaSen <giammasena@gmail.com>
2020-09-11 16:16:35 +0200

    land: copy vndk-29 libbinderthreadstate.so to vendor
    09-11 14:55:24.310  1517  1517 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "libbinderthreadstate.so" not found: needed by /vendor/lib64/libbindergx.so in namespace (default)

--- device.mk
+++ device.mk
@@ -453,6 +453,9 @@ PRODUCT_PACKAGES += \
     vndk_package \
     libstdc++.vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwpa_client
