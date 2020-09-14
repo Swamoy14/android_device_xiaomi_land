7405f2a7960429454218a5bf2da6833d63d066ff
xyyx <xyyx@mail.ru>
2020-09-14 09:47:50 +0000

    land: remove audio hal from init.qcom.rc
    also update entries in device.mk

--- device.mk
+++ device.mk
@@ -65,11 +65,15 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@6.0 \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.soundtrigger@2.2-service
 
