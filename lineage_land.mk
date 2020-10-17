0adc60bf4cb200fd5765e46f3669565413525f75
sairam1411 <majgaonkars3@gmail.com>
2020-10-17 09:41:20 +0530

    land: lineage bringup

--- lineage_land.mk
+++ lineage_land.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,7 +29,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := havoc_land
+PRODUCT_NAME := lineage_land
 PRODUCT_MODEL := Redmi 3S
 
 TARGET_BOOT_ANIMATION_RES := 720
@@ -42,7 +42,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM:user/release-keys
 
-export HAVOC_BUILD_TYPE=Official
-
-HAVOC_MAINTAINER := Sairam Majgaonkar (Sairam60)
-HAVOC_GROUP_URL := https://t.me/havoc_land
