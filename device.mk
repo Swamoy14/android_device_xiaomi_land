5969e6ff21bf2f2d003a19163b855a69ee9b4b84
sairam1411 <majgaonkars3@gmail.com>
2020-10-17 09:17:27 +0530

    land: Update thermal blobs from olive
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- device.mk
+++ device.mk
@@ -424,6 +424,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
