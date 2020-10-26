fbd8e21537c051d77bf76c4f1e27ff11ce3a83ae
Hyper <HyperServer@gmail.com>
2020-10-26 09:30:00 +0530

    land: Switch to BOARD_VENDOR_SEPOLICY_DIRS
    Signed-off-by: Hyper <HyperServer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,7 +213,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
