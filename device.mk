a5f3dc23821960a5361b54948c26c45058e02d23
Swamoy14 <shubhasankar14@gmail.com>
2020-11-18 19:28:58 +0530

    land: camera: Ship GCamGo instead of GCam

--- device.mk
+++ device.mk
@@ -122,7 +122,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8937 \
-    GCam
+    CameraGo
     
 # Charger
 include $(LOCAL_PATH)/rootdir/etc/charger/charger.mk
